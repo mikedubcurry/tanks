e3860cbe4d9977dbb4e5bb2cd52cf49bde032d23
mikedubcurry <fender18295@gmail.com>
2021-12-07 17:24:23 -0500

    env var for server IP

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { config as dotenvConfig } from 'dotenv';
+dotenvConfig();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
